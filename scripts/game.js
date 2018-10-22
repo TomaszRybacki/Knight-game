e02e7d4e7963c171cecb23c69ea88d17a595b07e
Tomasz89 <TomaszRybacki1989@gmail.com>
2018-10-22 21:09:22 +0200

    'base game setup' added

---
+++ scripts/game.js
@@ -0,0 +1,29 @@
+// Creating a game
+
+
+var gameCanvas = document.getElementById('game-canvas');
+var game = new Phaser.Game(1024, 448, Phaser.AUTO, gameCanvas, {
+	preload, create, update
+});
+
+
+// ********************************* Game status
+
+
+// Loading items
+
+function preload() {
+
+}
+
+// Creating elements
+
+function create() {
+
+}
+
+// Elements update
+
+function update() {
+
+}
