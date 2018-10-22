fe3ef5fada7fc26a87ed78a10461e43b323e70ae
Tomasz89 <TomaszRybacki1989@gmail.com>
2018-10-22 21:12:08 +0200

    'assets graphic' loaded

--- scripts/game.js
+++ scripts/game.js
@@ -13,7 +13,19 @@ var game = new Phaser.Game(1024, 448, Phaser.AUTO, gameCanvas, {
 // Loading items
 
 function preload() {
+  game.load.image('background', 'assets/background.jpg');
+	game.load.image('ground', 'assets/ground.png');
+	game.load.image('arrows', 'assets/arrows.png');
+	game.load.image('totem', 'assets/totem.png');
 
+	game.load.image('tile-1', 'assets/tile-1.png');
+	game.load.image('tile-2', 'assets/tile-2.png');
+	game.load.image('tile-3', 'assets/tile-3.png');
+	game.load.image('tile-4', 'assets/tile-4.png');
+
+	game.load.spritesheet('enemy', 'assets/enemy.png', 128, 128);
+	game.load.spritesheet('player', 'assets/knight.png', 128, 128);
+	game.load.spritesheet('gold', 'assets/gold.png', 64, 64);
 }
 
 // Creating elements
